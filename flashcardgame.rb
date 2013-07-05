6dba0f13d73cae197b32e1c8757a2b6ec9c68181
nathanielwroblewski <nathanielwroblewski@gmail.com>
2013-07-05 12:31:37 -0700

    Added initial flashcardgame.rb file and the provided flash card samples file

---
+++ flashcardgame.rb
@@ -0,0 +1,24 @@
+class FlashCard
+
+  attr_reader :term, :definition
+
+  def initialize(term, definition)
+    @term = term
+    @definition = definition
+  end
+
+end
+
+class Deck
+
+  def initialize
+    flashcards = []
+  end
+
+  def add_flashcard
+
+  end
+
+end
+
+card[term] == definition
