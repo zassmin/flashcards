13a623f6eeaf568e83d43283cce6498a1760874d
Maria Pacana <maria.pacana@gmail.com>
2013-07-05 16:25:24 -0700

    Not fully functional GameController

--- flashcardgame.rb
+++ flashcardgame.rb
@@ -35,3 +35,58 @@ class Deck
 
 end
 
+module GameView
+  attr_accessor :gameview_guess
+
+  def initialize
+    @gameview_guess = ''
+  end
+
+  def display_card_definition(card)
+    puts "#{card.definition}"
+  end
+
+  def input_guess
+    puts "Guess the term:"
+    @gameview_guess = gets.chomp
+  end
+
+  def correct_answer
+    puts "Correct!"
+  end
+
+  def incorrect_answer
+    puts "Incorrect!"
+  end
+
+end
+
+
+class GameController
+  include GameView
+
+  attr_accessor :deck
+  
+  def initialize
+    super
+  end
+
+  def create_game(deck)
+    @deck = deck
+  end
+
+  def grab_card
+    @graveyard_deck << deck.shift
+  end
+
+  def check_guess(guess)
+    if guess == @card.term
+      correct_answer
+    else
+      incorrect_answer
+      input_guess
+    end
+  end
+
+
+end
